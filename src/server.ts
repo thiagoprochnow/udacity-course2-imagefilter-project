167e8c2a2d0d2006287a73cf9550d11f5593bad3
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-08-02 17:55:05 -0300

    Clean up and add comments

--- src/server.ts
+++ src/server.ts
@@ -20,29 +20,17 @@ import { AuthRouter, requireAuth } from './user/routes/auth.router';
   app.use(bodyParser.json());
   app.use('/auth/',AuthRouter);
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
   app.get('/filteredimage/', requireAuth, async (req, res) => {
     let { image_url } = req.query;
 
+    // Verify if image url is set, if not, return 400
     if ( !image_url ) {
       return res.status(400)
                 .send(`image_url is required`);
     }
+
+    // Try to filter image with passed url, if success, returns image, else returns 500 error
     try{
       let file = await filterImageFromURL(image_url);
       res.status(200).sendFile(file, () => {
@@ -52,8 +40,6 @@ import { AuthRouter, requireAuth } from './user/routes/auth.router';
       res.status(500).send("Error getting image from URL")
     }
   } );
-
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
